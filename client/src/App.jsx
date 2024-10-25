ba7f4d65f5a1ef8c9903af7be38757cd49045cd5
Michael Fuentes <140119186+fuentesmichael1@users.noreply.github.com>
2024-10-24 19:29:21 -0500

    added bootstrap
    added bootstrap to have some fun with

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DisplayAll from './views/DisplayAll'
