d2cec0842e6abe6bee805a7f9f43d498c46d3898
Antonio Elias <AntonioE119@gmail.com>
2024-10-25 20:27:43 -0500

    Login/Reg
    Login/reg

--- server/models/like.model.js
+++ server/models/like.model.js
@@ -2,14 +2,9 @@ import {model, Schema} from 'mongoose';
 
 const LikeSchema = new Schema(
     {
-
-        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, 
-
-        post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' } 
-
-
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
     },
-
     { timestamps: true }
 );
 const Like = model("like", LikeSchema);
