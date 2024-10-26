7983b988f868dbd5f274e3e847d56de813edc38f
Chanakya Garikapati <chanu.garikapati@gmail.com>
2024-10-25 19:18:45 -0500

    Front End Reg
    Worked on front end Reg and commented out code from server.js and app.jsx for testing the Reg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,27 +6,42 @@ import DisplayUser from './views/DisplayUser'
 import Login from './views/Login'
 import Navbar from './components/Navbar'
 import './App.css'
+=======
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DisplayAll from "./views/DisplayAll";
+//import DisplayUser from "./views/DisplayUser";
+import Login from "./views/Login";
+import Navbar from "./components/Navbar";
+import Register from "./views/Register";
+import "./App.css";
+>>>>>>> Stashed changes
 
 function App() {
-  const [errors, setErrors] = useState({})
+  // const [errors, setErrors] = useState({});
 
-  const errorUpdater = (newErrors) => {
-    setErrors((newErrors))
-  }
+  //const errorUpdater = (newErrors) => {
+  //  setErrors(newErrors);
+  //};
 
   return (
     <BrowserRouter>
-    <div className="app-container">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login errors={errors} errorUpdater={errorUpdater}/>} />
-        <Route path="/bright_ideas" element={<DisplayAll />} />
-        <Route path="/bright_ideas/:id" element={<DisplayOne />} />
-        <Route path="/users/:id" element={<DisplayUser />} />
-      </Routes>
-    </div>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Register />} />
+          {/* <Route
+            path="/"
+            element={<Login errors={errors} errorUpdater={errorUpdater} />}
+          />*/}
+          <Route path="/bright_ideas" element={<DisplayAll />} />
+          {/*<Route path="/bright_ideas/:id" element={<DisplayOne />} />*/}
+          {/* <Route path="/users/:id" element={<DisplayUser />} /> */}
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/views/Register.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8000/api/register", {
+        firstName,
+        lastName,
+        email,
+        password,
+        confirmPassword,
+      });
+      navigate("/bright_ideas");
+    } catch (error) {
+      console.error("Registration error:", error);
+      alert("Registration failed. Please try again.");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+export default Register;
